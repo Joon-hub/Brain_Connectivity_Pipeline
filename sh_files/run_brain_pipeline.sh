32304b94355f34d8b0ec585cef0ab9a593ae9bd9
Sudhir Joon <sjoon@head2.htc.inm7.de>
2025-11-08 15:42:27 +0100

    code changed to sklearn trasnformer pipeline -- 2

--- sh_files/run_brain_pipeline.sh
+++ sh_files/run_brain_pipeline.sh
@@ -29,7 +29,7 @@ set -e
 source /home/sjoon/projects/brain_connectivity_classifier/masterthesis_venv/bin/activate
 
 # echo "Running: python run.py --config config.yaml --help"
-python3 run.py --config config.yaml --jobname $JOBNAME
+python3 run.py --config config.yaml 
 
 EXIT_CODE=$?
 
